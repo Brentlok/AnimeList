c39c4987c84f1dbc397e0c09fe0f99ce65455942
brentlok <brentlokk@gmail.com>
2022-08-21 11:05:04 +0200

    Initial

---
+++ src/server/router/anime.ts
@@ -0,0 +1,21 @@
+import { z } from "zod";
+import { createProtectedRouter } from "./protected-router";
+
+export const animeRouter = createProtectedRouter()
+  .query("hello", {
+    input: z
+      .object({
+        text: z.string().nullish(),
+      })
+      .nullish(),
+    resolve({ input }) {
+      return {
+        greeting: `Hello ${input?.text ?? "world"}`,
+      };
+    },
+  })
+  .query("getAll", {
+    async resolve({ ctx }) {
+      return await ctx.prisma.example.findMany();
+    },
+  });
