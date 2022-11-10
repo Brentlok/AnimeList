c6aad5bbab56fa701497fdf5b650b7c53526c9c2
Hubert Bieszczad <hubert.bieszczad@rad4m.com>
2022-11-10 15:07:02 +0100

    Create simple file uploader

--- src/bits/Input/index.ts
+++ src/bits/Input/index.ts
@@ -1,2 +1,3 @@
 export * from './Text';
 export * from './Stars';
+export * from './File';
--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -7,12 +7,13 @@ import { profileAtom } from "~/state";
 import { preventDefault, trpc } from "~/utils";
 
 const Profile = () => {
-    const { data: session } = useSession();
+    const { status } = useSession();
     const [profile, setProfile] = useAtom(profileAtom);
+    const [avatar, setAvatar] = useState(profile.avatar);
     const [name, setName] = useState(profile.name);
     const changeName = trpc.useMutation(['profile.changeName']);
 
-    if (!session?.user) {
+    if (status === 'unauthenticated') {
         return <h1>Login to view your profile</h1>;
     }
 
@@ -25,16 +26,34 @@ const Profile = () => {
         setProfile({ ...profile, name });
     }
 
+    const img = avatar === ''
+        ? (
+            <div className="w-24 h-24">
+                <Input.File setDataUrl={setAvatar} />
+            </div>
+        )
+        : (
+            <Image
+                className='rounded-full overflow-hidden'
+                src={avatar}
+                alt=''
+                width={96}
+                height={96}
+            />
+        );
+
     return (
         <div className="anime flex flex-col">
             <div className="p-4 flex gap-4 items-center">
-                <Image
-                    className='rounded-full overflow-hidden'
-                    src={profile.avatar}
-                    alt=''
-                    width={100}
-                    height={100}
-                />
+                <div className="relative">
+                    <div
+                        className="absolute -right-2 -top-2 cursor-pointer"
+                        onClick={() => setAvatar('')}
+                    >
+                        <div className="close scale-50 w-5 h-5" />
+                    </div>
+                    {img}
+                </div>
                 <form onSubmit={preventDefault(handleSubmit)}>
                     <h3 className="mb-1">User name</h3>
                     <Input.Text
